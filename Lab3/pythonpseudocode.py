d6befab58ca3f2debdc54322fd010ad3ff76927e
chmagee <chmagee@ucsc.edu>
2020-05-03 16:08:21 -0700

    Unfinished Lab3-committing x4 to avoid cheating

--- Lab3/pythonpseudocode.py
+++ Lab3/pythonpseudocode.py
@@ -11,22 +11,25 @@ while useIn <= 0 :
 
 #print("Nice.")
 
+# lines = number of lines in the triangle
 lines = useIn
+# stars = number of asterisks in the line
 stars = (lines - 1) * 2
+# num = number of 
 num = 0
+# count = numbers in each line (ex: 2,3,3,2)
 count = 0
+# total = total number of characters in a line
 total = useIn*2
 
-if lines != 0 :
+while lines != 0 :
     count = count + 1
     print (count, end = "\t")
     while stars > 0 :
         print ("*", end = "\t")
-        total = total - 2
-        lines = lines - 1
         stars = stars - 1
     # end while
     print (count)
     count = count - 1
     lines = lines - 1
-# end if
+    stars = (lines - 1) * 2
