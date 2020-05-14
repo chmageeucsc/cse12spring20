ca20717165988fa40a65540f44daaa4d0df39247
chmagee <chmagee@ucsc.edu>
2020-05-13 23:01:52 -0700

    Unfinished Lab4-committing x4 to avoid cheating

--- Lab4/4psuedo.py
+++ Lab4/4psuedo.py
@@ -1,20 +1,34 @@
 # Lab4 Pseudocode
 
-print('Program arguments:')
-inputs = 0
-args = []
+#print('Program arguments:')
+#inputs = 0
+#args = []
 
-while inputs != '' :
-    inputs = input('')
-    if inputs == '' :
-        continue
-    decimal = int(inputs, 16)
-    args.append(inputs)
+#while inputs != '' :
+#    inputs = input('')
+#    if inputs == '' :
+#        continue
+#    decimal = int(inputs, 16)
+#    decimal = str(decimal)
+#    args.append(inputs)
 # end while
 
-print(args)
-print()
-print('Integer values:')
+#print(args)
+#print()
+#print('Integer values:')
 
-for i in args :
-    
+#for i in len(args) :
+#    print(args[i])
+#    i += 1
+
+num = 0
+leest = []
+while num != '' :
+    num = input('')
+    if num == '':
+        continue
+    leest.append(num)
+print(leest)
+
+for i in leest :
+    print(int(i))
